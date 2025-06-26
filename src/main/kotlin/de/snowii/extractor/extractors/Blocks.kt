a3d5a19a384f3511567618037264dc2834db3a41
teknostom <evaldsson.tom@gmail.com>
2025-06-26 11:11:12 +0200

    add has_random_ticks

--- src/main/kotlin/de/snowii/extractor/extractors/Blocks.kt
+++ src/main/kotlin/de/snowii/extractor/extractors/Blocks.kt
@@ -145,6 +145,7 @@ class Blocks : Extractor.Extractor {
                 stateJson.addProperty("luminance", state.luminance)
                 stateJson.addProperty("piston_behavior", state.pistonBehavior.name)
                 stateJson.addProperty("hardness", state.getHardness(null, null))
+                stateJson.addProperty("has_random_ticks", block.hasRandomTicks(state))
                 if (state.isOpaque) {
                     stateJson.addProperty("opacity", state.opacity)
                 }
