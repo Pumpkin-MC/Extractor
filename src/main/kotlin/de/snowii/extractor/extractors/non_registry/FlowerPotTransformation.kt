5da9f2f2933f270361404e0da2ad3dea48924a1e
ht06 <hthug06@gmail.com>
2025-06-29 12:21:38 +0200

    fix: use item id instead of item name

--- src/main/kotlin/de/snowii/extractor/extractors/non_registry/FlowerPotTransformation.kt
+++ src/main/kotlin/de/snowii/extractor/extractors/non_registry/FlowerPotTransformation.kt
@@ -2,23 +2,24 @@ package de.snowii.extractor.extractors.non_registry
 
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
 import de.snowii.extractor.Extractor
 import net.minecraft.block.FlowerPotBlock
 import net.minecraft.registry.Registries
 import net.minecraft.server.MinecraftServer
 
-class FlowerPot : Extractor.Extractor {
+class FlowerPotTransformation : Extractor.Extractor {
     override fun fileName(): String {
-        return "flower_pot.json"
+        return "flower_pot_transformations.json"
     }
 
     override fun extract(server: MinecraftServer): JsonElement {
         val flowerPotsJson = JsonObject()
         for ((block, pottedBlock) in FlowerPotBlock.CONTENT_TO_POTTED){
             if (Registries.BLOCK.getRawId(block) == 0) continue
-            flowerPotsJson.addProperty(
-                Registries.BLOCK.getId(block).toString(),
-                Registries.BLOCK.getId(pottedBlock).toString())
+            flowerPotsJson.add(
+                Registries.ITEM.getRawId(block.asItem()!!).toString(),
+                JsonPrimitive(Registries.BLOCK.getRawId(pottedBlock)))
         }
 
         return flowerPotsJson
