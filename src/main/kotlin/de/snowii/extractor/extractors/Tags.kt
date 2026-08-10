b727bfaaa189f86f9d5a592348f1ee0e92633799
MegalithOfficial <gekocakaya@gmail.com>
2026-08-10 21:40:13 +0200

    feat: extract configured feature tags

--- src/main/kotlin/de/snowii/extractor/extractors/Tags.kt
+++ src/main/kotlin/de/snowii/extractor/extractors/Tags.kt
@@ -4,6 +4,7 @@ import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import de.snowii.extractor.Extractor
+import net.minecraft.core.registries.Registries
 import net.minecraft.server.MinecraftServer
 import net.minecraft.tags.TagNetworkSerialization
 
@@ -36,6 +37,20 @@ class Tags : Extractor.Extractor {
             }
             tagsJson.add(registryKey.identifier().path, tagGroupTagsJson)
         }
+
+        val configuredFeatures = registryAccess.lookupOrThrow(Registries.CONFIGURED_FEATURE)
+        val configuredFeatureTagsJson = JsonObject()
+        configuredFeatures.listTags().forEach { tag ->
+            val values = JsonArray()
+            tag.forEach { holder ->
+                values.add(holder.unwrapKey().orElseThrow().identifier().path)
+            }
+            configuredFeatureTagsJson.add(tag.key().location().toString(), values)
+        }
+        tagsJson.add(
+            Registries.CONFIGURED_FEATURE.identifier().path,
+            configuredFeatureTagsJson
+        )
 
         return tagsJson
     }
